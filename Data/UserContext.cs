b76f02a42bd13352799d9f265f12d66d40d57762
Your Coworker <your.coworker@woventeams.com>
2022-10-08 10:05:10 +0200

    Add User model

---
+++ Data/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using audits_.net_react_ramine.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace audits_.net_react_ramine.Data;
+
+public class UserContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+	public UserContext(DbContextOptions<UserContext> options) : base(options)
+	{
+	}
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().ToTable("users");
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace audits_.net_react_ramine.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Password { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool DailyEmailUpdates { get; set; }
+}
